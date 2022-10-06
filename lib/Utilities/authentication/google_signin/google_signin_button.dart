2fc97878cbd3d40fd5c738563e54a87b5d098811
Long Trần <longtran@Longs-MacBook-Pro.local>
2022-10-06 14:37:43 +0700

    update utilities

--- lib/Utilities/authentication/google_signin/google_signin_button.dart
+++ lib/Utilities/authentication/google_signin/google_signin_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../Widgets/animated_button.dart';
+import '../../../Widgets/loading_error.dart';
 import 'controller/google_signin_controller.dart';
 
 class GoogleSignInButton extends ConsumerWidget {
@@ -11,41 +13,62 @@ class GoogleSignInButton extends ConsumerWidget {
       googleSignInProvider,
       (previous, current) {
         if (current == GoogleSignInState.loading) {
-          showDialog(
-            context: context,
-            builder: (_) => const SimpleDialog(
-              title: Padding(
-                padding: EdgeInsets.all(32.0),
-                child: Text("Loading..."),
-              ),
-            ),
-          );
+          LoadingSheet.show(context);
         } else if (current == GoogleSignInState.error) {
           Navigator.of(context).pop();
-
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text("Google signin failed"),
-            ),
-          );
+          ErrorDialog.show(context, "Google signin failed!");
         } else {
           Navigator.of(context).pop();
         }
       },
     );
 
-    return TextButton(
-      child: const Text(
-        "Sign In With Google",
-        style: TextStyle(
-          color: Color(0xFF9A9A9A),
-          fontWeight: FontWeight.bold,
-          fontSize: 18,
-        ),
-      ),
-      onPressed: () {
+    return AnimatedButton(
+      onTap: () {
         ref.read(googleSignInProvider.notifier).signInWithGoogle();
       },
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 18),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Colors.black.withOpacity(0.2),
+            width: 1.5,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              'http://pngimg.com/uploads/google/google_PNG19635.png',
+              width: 30,
+              height: 30,
+            ),
+            const Text(
+              "Sign in with Google",
+              style: TextStyle(
+                color: Color(0xFF9A9A9A),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+    // TextButton(
+    //   child: const Text(
+    //     "Sign In With Google",
+    //     style: TextStyle(
+    //       color: Color(0xFF9A9A9A),
+    //       fontWeight: FontWeight.bold,
+    //       fontSize: 18,
+    //     ),
+    //   ),
+    //   onPressed: () {
+    //     ref.read(googleSignInProvider.notifier).signInWithGoogle();
+    //   },
+    // );
   }
 }
