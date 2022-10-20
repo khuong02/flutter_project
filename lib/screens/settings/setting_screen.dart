8e4f9efc7d5554dc59ffaaeaf49a4f9b942ff588
dduyen <duyendinh2021@outlook.com>
2022-10-20 19:41:56 +0700

    add setting page

--- lib/screens/settings/setting_screen.dart
+++ lib/screens/settings/setting_screen.dart
@@ -1,20 +1,136 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:babstrap_settings_screen/babstrap_settings_screen.dart';
+import 'package:line_icons/line_icons.dart';
 
-class SettingScreen extends StatefulWidget{
+class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _SettingScreen();
 }
 
-class _SettingScreen extends State<SettingScreen>{
-
+class _SettingScreen extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Setting screen"),
+    return Scaffold(
+      backgroundColor: Colors.white.withOpacity(.94),
+      appBar: AppBar(
+        title: Text(
+          "Settings",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            SettingsGroup(
+              items: [
+                SettingsItem(
+                  onTap: () {},
+                  icons: LineIcons.history,
+                  iconStyle: IconStyle(),
+                  title: 'card recharge history',
+                  // subtitle: "Make Ziar'App yours",
+                ),
+                SettingsItem(
+                  onTap: () {},
+                  icons: Icons.fingerprint,
+                  iconStyle: IconStyle(
+                    iconsColor: Colors.white,
+                    withBackground: true,
+                    backgroundColor: Colors.red,
+                  ),
+                  title: 'Privacy',
+                  subtitle: "Lock Ziar'App to improve your privacy",
+                ),
+                SettingsItem(
+                  onTap: () {},
+                  icons: Icons.notifications,
+                  iconStyle: IconStyle(
+                    iconsColor: Colors.white,
+                    withBackground: true,
+                    backgroundColor: Colors.orange,
+                  ),
+                  title: 'Notification',
+                  subtitle: "Invitation, ...vv",
+                  trailing: Switch.adaptive(
+                    value: false,
+                    onChanged: (value) {},
+                  ),
+                ),
+                SettingsItem(
+                  onTap: () {},
+                  icons: CupertinoIcons.bell,
+                  iconStyle: IconStyle(
+                    iconsColor: Colors.white,
+                    withBackground: true,
+                    backgroundColor: Colors.red,
+                  ),
+                  title: 'Sound',
+                  subtitle: "Invitation, ...vv",
+                  trailing: Switch.adaptive(
+                    value: false,
+                    onChanged: (value) {},
+                  ),
+                ),
+                SettingsItem(
+                  onTap: () {},
+                  icons: Icons.feedback_rounded,
+                  iconStyle: IconStyle(
+                    iconsColor: Colors.white,
+                    withBackground: true,
+                    backgroundColor: Colors.yellow,
+                  ),
+                  title: 'Send Feedback',
+                  subtitle: "send feedback about your experience,...",
+                ),
+              ],
+            ),
+            SettingsGroup(
+              items: [
+                SettingsItem(
+                  onTap: () {},
+                  icons: Icons.info_rounded,
+                  iconStyle: IconStyle(
+                    backgroundColor: Colors.purple,
+                  ),
+                  title: 'About',
+                  subtitle: "Learn more about Ziar'App",
+                ),
+              ],
+            ),
+            // You can add a settings title
+            SettingsGroup(
+              settingsGroupTitle: "Account",
+              items: [
+                SettingsItem(
+                  onTap: () {},
+                  icons: Icons.exit_to_app_rounded,
+                  title: "Sign Out",
+                ),
+                SettingsItem(
+                  onTap: () {},
+                  icons: CupertinoIcons.repeat,
+                  title: "Change email",
+                ),
+                SettingsItem(
+                  onTap: () {},
+                  icons: CupertinoIcons.delete_solid,
+                  title: "Delete account",
+                  titleStyle: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
