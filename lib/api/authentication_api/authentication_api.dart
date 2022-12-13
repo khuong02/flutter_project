22f5e9c8ba8177a6f5f0e7b076ef5b68708042f3
DuyenDinh <85270015+duyendinh2021@users.noreply.github.com>
2022-12-13 17:33:07 +0700

    update forgot password (#58)

--- lib/api/authentication_api/authentication_api.dart
+++ lib/api/authentication_api/authentication_api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:do_an_di_dong/Widgets/auth/signin/forgot_password_button.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
@@ -42,6 +43,18 @@ class AuthenticationApi {
       body: jsonEncode(data),
     );
     return response;
+  }
+
+  //forgot pass
+  ForgotPassword(Map<String, dynamic> data) async {
+    final rep = await http.post(
+      Uri.parse(Constants.urlApi + 'reset-password-request'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(data),
+    );
+    return rep;
   }
 
   static onSignOut(context) async {
--- lib/features/authentication/forgot_password/forgot_password_controller.dart
+++ lib/features/authentication/forgot_password/forgot_password_controller.dart
@@ -1,8 +1,9 @@
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:do_an_di_dong/api/authentication_api/authentication_api.dart';
+import 'package:do_an_di_dong/features/authentication/authentication_controller.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:form_validators/form_validators.dart';
-
 import '../../../Providers/auth_repo_provider.dart';
 
 part 'forgot_password_state.dart';
@@ -16,7 +17,6 @@ final forgotPasswordProvider = StateNotifierProvider.autoDispose<
 
 class ForgotPasswordController extends StateNotifier<ForgotPasswordState> {
   final AuthenticationRepository _authenticationRepository;
-
   ForgotPasswordController(this._authenticationRepository)
       : super(const ForgotPasswordState());
 
@@ -35,7 +35,11 @@ class ForgotPasswordController extends StateNotifier<ForgotPasswordState> {
     if (!state.status.isValidated) return;
     state = state.copyWith(status: FormzStatus.submissionInProgress);
     try {
-      await _authenticationRepository.forgotPassword(email: state.email.value);
+      Map<String, dynamic> data = {
+        'email': state.email.value,
+        'roles': 0,
+      };
+      await AuthenticationApi().ForgotPassword(data);
       state = state.copyWith(status: FormzStatus.submissionSuccess);
     } on ForgotPasswordFailure catch (e) {
       state = state.copyWith(
